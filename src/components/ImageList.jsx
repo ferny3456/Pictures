2725eaa493031b27d3362bc6893b0669719c5462
ferny3456 <rochafernanda1123@gmail.com>
2025-04-08 17:53:17 -0700

    Actualice ImageShow.jsx y ImageList.jsx

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,7 +1,16 @@
-const ImageList = () => {
+import ImageShow from "./ImageShow";
+import './ImageList.css'
+
+const ImageList = ({images}) => {
+    const renderImage = images.map((image) => {
+        return <ImageShow key={image.id} image={image} />
+
+    })
     return (
-      <div>ImageList</div>
+        <div className="Image.list">
+            {renderImage}
+        </div>
     )
-  }
-  
-  export default ImageList
+}
+
+export default ImageList
